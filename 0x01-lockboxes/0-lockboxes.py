9a418edde046323d2c693d1bb55d56d48bc1cd7b
Bigkhil <khm40005000@outlook.com>
2024-10-16 23:07:33 +0300

    some changes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -3,19 +3,23 @@
 check if all the boxes are unlocked or not
 """
 
+
 def canUnlockAll(boxes):
     """this function should return if the boxes can be opened or not"""
+    if len(boxes) == 0:
+        return False
+
     vis = [0] * len(boxes)
+    stack = [0]
+    vis[0] = 1
     cntr = 1
-    def dfs(st):
-        """this is depth first search algorithm"""
-        nonlocal cntr
-        vis[st] = 1
+
+    while stack:
+        st = stack.pop()
         for neg in boxes[st]:
-            if (neg < len(boxes) and not vis[neg]):
+            if neg < len(boxes) and not vis[neg]:
+                vis[neg] = 1
                 cntr += 1
-                dfs(neg)
-        return cntr
-    if dfs(0) == len(boxes):
-        return True
-    return False
+                stack.append(neg)
+
+    return cntr == len(boxes)
