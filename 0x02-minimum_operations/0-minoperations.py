8c03c3d822fac07ae5a58668cb9797be294e9d51
Bigkhil <khm40005000@outlook.com>
2024-10-17 22:23:02 +0300

    some changes

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -8,12 +8,12 @@ def minOperations(n):
     """function to return the min no of operations"""
     if n <= 1:
         return 0
-    
+
     sum = 0
-    for i in range (2, n + 1):
+    sup = n
+    for i in range(2, sup + 1):
         while (n % i == 0):
             sum += i
-            n = n // i
+            n = n / i
 
     return sum
-    
